10ac0b2c3f7a99e035667820d4ddd6b5b1d8e798
François Ménabé <francois.menabe@gmail.com>
2015-06-17 17:44:35 +0200

    Set version to 0.1.0.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='clif',
-    version='0.0.1',
+    version='0.1.0',
     author='François Ménabé',
     author_email='francois.menabe@gmail.com',
     license='MIT License',
