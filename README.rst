070d73c1ff9516c7829c98ed28a3da19b52f1662
François Ménabé <francois.menabe@gmail.com>
2016-12-06 18:00:41 +0100

    Reorganize 'clif' project.
    'clif' project, which regroup a few "plugins" for 'clg', has been split
    to multiple projects.
    
    This repository now contains only the 'splitup' plugin which aims (in the
    future) to manage logging based on command-line options.
    
    History has been cleaned from other plugins!

--- README.rst
+++ README.rst
@@ -1,6 +1,2 @@
-Command-line interface framework
-================================
-
-This is a small framework for generating command-line in python. Each command
-has a dedicated file for the command-line configuration and can have dedicated
-configuration files whose values will be easily accessible.
+python-clg-logger
+=================
--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
-    name='clif',
-    version='0.2.2',
+    name='clg-logger',
+    version='0.1.0',
     author='François Ménabé',
     author_email='francois.menabe@gmail.com',
-    url = 'http://github.com/fmenabe/python-clif',
-    download_url = 'http://github.com/fmenabe/python-clif',
+    url = 'https://clg.readthedocs.org/en/latest/',
+    download_url = 'http://github.com/fmenabe/python-clg-logger',
     license='MIT License',
-    description='Framework for generating command-line',
+    description='Manage logging based on command-line options.',
     long_description=open('README.rst').read(),
-    keywords=['command-line', 'argparse', 'wrapper', 'clg', 'framework'],
+    keywords=['command-line', 'argparse', 'wrapper', 'clg', 'logging'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
@@ -21,8 +21,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Utilities'
     ],
-    packages=['clif'],
-    install_requires=['clg', 'pyyaml', 'yamlordereddictloader'])
+    py_modules=['clg/logger.py'],
+    install_requires=['clg'])
